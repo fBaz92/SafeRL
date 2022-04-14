8ea797989a697320d60db734a72ed370e829e8d7
Francesco Bazzani <francesco.bazzani@me.com>
2022-04-14 11:51:42 +0200

    Bugfix of test.py and custom_gym_loop.py

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 # Suppress TF2 warnings and load libraries
-import os; os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-import gym, sys
+import os
+import gym
+import sys
+import light_safety_gym
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+#env = gym.make("light_safety_gym:point_discrete-v0")
+env = gym.make("point_discrete-v0")
 
 # Load the PPO algorithm for discrete action space and start the training
-def main_discrete_PPO( env, verbose=1, episodes=5000, render=False ):
+def main_discrete_PPO(env, verbose=1, episodes=5000, render=False):
 	from algos.PPO import PPO
 
 	learner = PPO( env=env, verbose=verbose, render=render )
-	learner.loop( episodes )
+	learner.loop(episodes)
 
 
 # Load the IPO algorithm for discrete action space and start the training
@@ -29,12 +34,13 @@ def main_discrete_Lagrangian( env, verbose=1, episodes=5000, render=False ):
 
 # Parse the input command and run the correct function
 if __name__ == "__main__":
-	env = gym.make( "light_safety_gym:point_discrete-v0" )
-	main_discrete_PPO( env, verbose=1, episodes=5000 )
-	main_discrete_Lagrangian( env, verbose=1, episodes=5000 )
+	#env = gym.make("light_safety_gym:point_discrete-v0")
+	env = gym.make("point_discrete-v0")
+	main_discrete_PPO(env, verbose=1, episodes=5000)
+	main_discrete_Lagrangian(env, verbose=1, episodes=5000)
 	quit()
 
 	if (sys.argv[1] == "-PPO"): main_discrete_PPO()
 	elif (sys.argv[1] == "-IPO"): main_discrete_IPO()
 	elif (sys.argv[1] == "-Lagr"): main_discrete_Lagrangian()
-	else: raise ValueError(f"Invalid command: '{sys.argv[1]}' (options: [-PPO, -IPO, -Lagr])")
+	else: raise ValueError("Invalid command: {} (options: [-PPO, -IPO, -Lagr])".format(sys.argv[1]))
